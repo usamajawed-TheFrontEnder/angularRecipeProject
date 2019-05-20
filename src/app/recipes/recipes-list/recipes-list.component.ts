d7d62c01bc7cd2fe090fc532d708a2153594a164
usama jawed <usamajawed@outlook.com>
2019-05-20 15:29:45 +0500

    First commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
