493486850b75fdbee1648c333c4b5b926ca7b97e
usama jawed <usamajawed@outlook.com>
2019-05-20 16:16:39 +0500

    this is my second commit

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://s1.cdn.autoevolution.com/images/news/gallery/ferrari-hybrid-supercar-confirmed-by-ceo-slots-above-812-superfast_1.jpg')
+  ];
 
   constructor() { }
 
